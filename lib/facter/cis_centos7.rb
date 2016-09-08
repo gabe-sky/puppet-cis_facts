822e4900d3c2825de60ecb1d133fc8f060094a6a
Trev Harmon <trevharmon@gmail.com>
2016-09-08 11:59:54 -0600

    create an aggregate result set
    Even though we have each of the sections in their own library file, we
    want the resulting fact to be a single hash instead of multiple hashes.

--- lib/facter/cis_centos7.rb
+++ lib/facter/cis_centos7.rb
@@ -1,28 +1,53 @@
 $:.unshift '/opt/puppetlabs/puppet/cache/lib/facter/util'
 require 'CIS_1_CentOS7'
+require 'CIS_2_CentOS7'
+require 'CIS_3_CentOS7'
+require 'CIS_4_CentOS7'
+require 'CIS_5_CentOS7'
+require 'CIS_6_CentOS7'
 
-Facter.add(:cis_1_centos7) do
+Facter.add(:cis_centos7, :type => :aggregate) do
   confine :operatingsystem => 'CentOS'
   confine :operatingsystemmajrelease => '7'
 
   # Initialize test results
-  cis = CIS_1_CentOS7.new
+  cis_1 = CIS_1_CentOS7.new
+  cis_2 = CIS_2_CentOS7.new
+  cis_3 = CIS_3_CentOS7.new
+  cis_4 = CIS_4_CentOS7.new
+  cis_5 = CIS_5_CentOS7.new
+  cis_6 = CIS_6_CentOS7.new
 
   ########################################
   # Show full results
   #
-  results = CIS_1_CentOS7.facter_full_hash
+  results_1 = CIS_1_CentOS7.facter_full_hash
+  results_2 = CIS_2_CentOS7.facter_full_hash
+  results_3 = CIS_3_CentOS7.facter_full_hash
+  results_4 = CIS_4_CentOS7.facter_full_hash
+  results_5 = CIS_5_CentOS7.facter_full_hash
+#  results_6 = CIS_6_CentOS7.facter_full_hash
 
   ########################################
   # Show only test result
   #
-#  results = CIS_1_CentOS7.facter_result_hash
+#  results_1 = CIS_1_CentOS7.facter_result_hash
+#  results_2 = CIS_2_CentOS7.facter_result_hash
+#  results_3 = CIS_3_CentOS7.facter_result_hash
+#  results_4 = CIS_4_CentOS7.facter_result_hash
+#  results_5 = CIS_5_CentOS7.facter_result_hash
+#  results_6 = CIS_6_CentOS7.facter_result_hash
 
   ########################################
   # Show test title and result
   #
-#  keys = [:title, :result]
-#  results = CIS_1_CentOS7.facter_hash(keys)
+#  keys = [ :title, :result ]
+#  results_1 = CIS_1_CentOS7.facter_hash(keys)
+#  results_2 = CIS_2_CentOS7.facter_hash(keys)
+#  results_3 = CIS_3_CentOS7.facter_hash(keys)
+#  results_4 = CIS_4_CentOS7.facter_hash(keys)
+#  results_5 = CIS_5_CentOS7.facter_hash(keys)
+#  results_6 = CIS_6_CentOS7.facter_hash(keys)
 
   ########################################
   # Show custom results:
@@ -32,12 +57,32 @@ Facter.add(:cis_1_centos7) do
   # - :result := test result
   # - :exec   := test executables
   # - :data   := test executables' output (STDOUT)
-#  keys = [:title, :result, :exec, :data]
-#  results = CIS_1_CentOS7.facter_hash(keys)
+  keys = [ :title, :scored, :level, :result, :exec ]
+#  results_1 = CIS_1_CentOS7.facter_hash(keys)
+#  results_2 = CIS_2_CentOS7.facter_hash(keys)
+#  results_3 = CIS_3_CentOS7.facter_hash(keys)
+#  results_4 = CIS_4_CentOS7.facter_hash(keys)
+#  results_5 = CIS_5_CentOS7.facter_hash(keys)
+  results_6 = CIS_6_CentOS7.facter_hash(keys)
 
   # All set here, let's let Facter have it
-  setcode do
-    results
+  chunk(:one) do
+    results_1
+  end
+  chunk(:two) do
+    results_2
+  end
+  chunk(:three) do
+    results_3
+  end
+  chunk(:four) do
+    results_4
+  end
+  chunk(:five) do
+    results_5
+  end
+  chunk(:six) do
+    results_6
   end
 
 end
